769f3f0eb2a913becb5392982d1721a2dc273383
Jessica Ho <jess.c.ho@gmail.com>
2012-01-23 20:20:26 -0800

    Initial commit of BillBadger app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BillBadger::Application.config.secret_token = '7d4eb7304755533b2dc120b5120a974b976efd9e549984166cbe4536fd372b63758d7d1b649411d539d81c6c9a592db8268a30952168c02dc5937e20876d141b'
